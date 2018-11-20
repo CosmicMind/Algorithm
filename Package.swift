09f35064fde9d9d42a91dbf01f35de32d763ec77
Kyle Maxwell <kyle@kylemaxwell.com>
2018-11-20 12:07:59 -0800

    Add SPM Support

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Algorithm",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Algorithm",
+            targets: ["Algorithm"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Algorithm",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "AlgorithmTests",
+            dependencies: ["Algorithm"],
+            path: "Tests"),
+    ]
+)
